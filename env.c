a29c3035781839f4aecf792a01c5b3ea4a205c9f
Lin <lsun@student.hive.fi>
2023-04-17 11:27:43 +0300

    update env functions

--- env.c
+++ env.c
@@ -27,35 +27,9 @@ int env_init(t_mini *mini, char **env)
 			ft_lstadd_back(&mini->env, node);
 		i++;
 	}
-	test_env_functions(mini);
+	//test_env_functions(mini);
 	return (1);
-}
-
-void test_env_functions(t_mini *mini)
-{
-	//test env_handler
-	printf("\nenv handler: %s\n", env_handler(mini->env, "HOME"));
-	printf("\nenv handler: %s\n", env_handler(mini->env, "NONE"));
-	// test env_converter
-	char** test = env_convert(mini->env);
-	printf("\n%s\n%s\n%s\n%s\n", test[0], test[1], test[2], test[3]);
-
-	//ft_printf("\nbefore\n\n");
-	//my_env(NULL, mini->env);
-
-
-	//char *str[1024] = {"KEY=12345", NULL};
-	//my_export(str);
-	//ft_printf("\nafter\n");
-	//my_env(NULL);
-	//char **str2 = ft_split("KEY=12345", '=');
-	//ft_printf("\nafter unset\n");
-	//my_unset(str2);
-	//my_env(NULL);
-	//env_convert(mini->env);
-	//printf("%s\n", env_handler(mini->env, "HOME"));
-
-}
+} 
 
 /*
 ** by giving the name of the enviromental varible, such as HOME
@@ -75,9 +49,7 @@ char* env_handler(t_list *env, char* str)
 		env_content = (char**)tmp->content;
 		len = ft_strlen(env_content[0]);
 		//printf("name=%s len=%d\n", env_content->env_name, len);
-		if (len < ft_strlen(str))
-			len = ft_strlen(str);
-		if (ft_strncmp(str, env_content[0], len) == 0)
+		if (len == ft_strlen(str) && ft_strncmp(str, env_content[0], len) == 0)
 			return(env_content[1]);
 		tmp = tmp->next;
 	}
@@ -121,4 +93,29 @@ char** env_convert(t_list* env)
 		i++;
 	}
 	return(ret);
+}
+
+/* this function is used to update the enviromental varible*/
+void env_find_and_replace(t_list *env, char* to_find, char* to_replace)
+{
+	char** env_content;
+	t_list *tmp;
+	int len;
+
+	tmp = env;
+	env_content = (char**)env->content;
+	while (tmp)
+	{
+		env_content = (char**)tmp->content;
+
+		len = ft_strlen(env_content[0]);
+		if (len == ft_strlen(to_find) && ft_strncmp(to_find, env_content[0], len) == 0)
+		{
+			//ft_printf("now I found:%s\n", env_content[1]);
+			free(env_content[1]);
+			env_content[1] = ft_strdup(to_replace);
+			//ft_printf("after replace:%s\n", env_content[1]);
+		}
+		tmp = tmp->next;
+	}
 }
