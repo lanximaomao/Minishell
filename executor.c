95e581d62cdb9cc6d6938854bbe3a9bb772727ce
lanximaomao <linlin.sun@outlook.com>
2023-04-21 00:05:32 +0300

    executor without testing.

--- executor.c
+++ executor.c
@@ -23,63 +23,88 @@ int run_run_run(t_mini *mini)
 	int *pid;
 	int *status;
 	int size;
+	int **fd_pipe;
 
 	i = 0;
 	size = ft_lstsize(mini->cmd_lst);
 	pid = malloc(sizeof(int) * size);
 	if (!pid)
 		ft_error("malloc fail.\n", 1);
+	// why would I need status?
 	status = malloc(sizeof(int) * size);
 	if (!status)
 		ft_error("malloc fail.\n", 1);
+	//init fd for creating pipes
+	fd_pipe = malloc(sizeof(int*) * (size - 1));
+	if (!fd_pipe)
+		ft_error("malloc fail.\n", 1);
+	while (i < size - 1)
+	{
+		fd_pipe[i] = malloc(sizeof(int) * 2);
+		if (!fd[pipe[i]])
+			ft_error("malloc fail.\n", 1);
+		i++;
+	}
+	//create size - 1 pipes
+	handel_pipe_create(fd_pipe, size);
 	tmp = mini->cmd_lst;
-	while (mini->cmd_lst)
+	while (mini->cmd_lst && i < size)
 	{
 		pid[i] = fork();
 		if (pid[i] == -1)
 			ft_error("fork failed.\n", 4);
 		else if (pid[i] == 0)
-			cmd(mini->cmd_lst, line); //node
+			cmd((t_token*)mini->cmd_lst->content, fd_pipe, size, i);
 		tmp = tmp->next;
+		i++;
 	}
 	i = 0;
 	while(i++ < size)
 		waitpid(pid1, &status[i], 0);
 }
 
-int cmd(t_mini *mini, char* line)
+int cmd(t_token* token, int** fd_pipe, int size, int which_pipe)
 {
-	t_token *token;
-
-	token = (t_token*)(mini->cmd_lst);
-
+	// close unused pipes
+	handel_pipe_close(fd_pipe, size, which_pipe);
 	//handel input and output
-	handel_file(mini->cmd_lst);
+	handel_file(token, fd_pipe, size, which_pipe);
+	// handel in and out
+	//dup2(token->fd_in, 0);
+	//dup2(token->fd_out, 1);
+	//close other fd, should I close fd_in when it is 0 and fd_out when it is 1???
+	if (close(token->fd_in) == -1 || close(token->fd_out) == -1)
+		ft_error("file cannot be closed.\n", 4);
+	if (close(fd_pipe[i][0]) == -1 || close(fd_pipe[i][1]) == -1)
+		ft_error("file cannot be closed.\n", 4);
 
 	// check if it is a buildin function, if yes, run and return.
 	if (is_buildin(token->args, mini->env) == 1)
 		return;
 	// not a buildin, check if full path the already available, if yes, run and return.
 	if (access(token->args[0], X_OK) == 0)
-		execve(token->args[0], token->args, env_convert(mini->env));
+	{
+		if (execve(token->args[0], token->args, env_convert(mini->env)) == -1)
+			ft_error("Cannot execute command.\n", 4);
+	}
 	else
 	{
 		tmp = ft_strjoin("/", token->args[0]);//to be freed
 		path_cmd = get_path_cmd(tmp, mini->env);
 		free(tmp);
-		execve(path_cmd, cmd_args, env_convert(mini->env)); // !error return
+		if (execve(path_cmd, cmd_args, env_convert(mini->env)) == -1)
+			ft_error("Cannot execute command.\n", 4); // !error return
 	}
 	return(0);
 }
 
 /* in order to save the fd_in and fd_out, add definition at s_token struct*/
-int handel_file(t_list *cmd_lst)
+// should use dup2 instead of open and close!!
+int handel_file(t_token* token, int** fd_pipe, int which_pipe, int size)
 {
-	t_token *token;
 	int i;
 
 	i = 0;
-	token = (t_token *)cmd_lst->content;
 	// loop through each infile
 	while (token->infile[i])
 	{
@@ -93,14 +118,23 @@ int handel_file(t_list *cmd_lst)
 			close(token->fd_in);
 		i++;
 	}
-	// if no infile is given, by default it should take stdin
+	// if no infile is given,
+	// by default it should take stdin if it is the first cmd
+	// or reading from the connected pipes
 	if (token->infile[0] == NULL)
-		token->fd_in = 0;
-	//loop through each outfile
+	{
+		if (which_pipe == 0)
+			token->fd_in = 0;
+		else
+			token->fd_in = fd[i][0];
+	}
 	i = 0;
 	while (token->outfile[i])
 	{
-		token->fd_out = open(token->outfile[i], O_WRONLY | O_CREAT|O_TRUNC, 0644);
+		if (token->output_type[i] == 2) // append mode
+			token->fd_out = open(token->outfile[i], O_WRONLY | O_CREAT|O_APPEND, 0644);
+		else
+			token->fd_out = open(token->outfile[i], O_WRONLY | O_CREAT|O_TRUNC, 0644);
 		if (token->fd_out == -1)
 		{
 			perror("Fail to create or open outfile");//exit or not?
@@ -110,10 +144,52 @@ int handel_file(t_list *cmd_lst)
 			close(token->fd_out);
 		i++;
 	}
-	// if no outfile is given, by default it should be stdout
-	if (token->outfile[0])
-		token->fd_out = 1;
+	// if no outfile is given,
+	// by default it should be stdout if it's the last cmd
+	// otherwise, it should redirect to the connected pipe
+	if (token->outfile[0] == NULL)
+	{
+		if (which_pipe == size - 1 - 1)
+			token->fd_out = 1;
+		else
+			token->fd_out = fd_pipe[i][1];
+	}
 	return(0);
+}
+
+void handel_pipe_create(int** fd_pipe, int size)
+{
+	int i;
+	int ** fd_pipe;
+
+	//set up pipe
+	i = 0;
+	while (i < size - 1)
+	{
+		if (pipe(fd_pipe[i]) == -1)
+			ft_error("error in creating pipes.\n", 4);
+		i++;
+	}
+}
+
+// this will be used in each children processor to close all the irrelevant pipes.
+int handel_pipe_close(int **fd_pipe, int size, int which_pipe)
+{
+	// for pipe[i], what should I close?
+	int i;
+
+	i = 0;
+	while ( i < size - 1)
+	{
+		if ( i != which_pipe)
+		{
+			if (fd[i][0] > 0 && close(fd[i][0]) == -1)
+				ft_error("file cannot be closed.", 4);
+			if (fd[i][1] > 0 && close(fd[i][1]) == -1)
+				ft_error("file cannot be closed.", 4);
+		}
+		i++;
+	}
 }
 
 char* get_path_cmd(char* str, t_list *env)
